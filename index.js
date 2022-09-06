0fd5938f0ba383992bd9648fbc5fbbc5ef350182
Erica2012 <mcrenoldj@gmail.com>
2022-09-06 10:12:24 +0000

    my gpmiddleware

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const userRouter = require("./routes/users.route")
+const {globalMiddleware} = require("./middlewares/globalMiddleware")
 
-const app = express()
+const app = express();
+app.use(globalMiddleware);
 
 app.get("/", (req, res) => {
     res
